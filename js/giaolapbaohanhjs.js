7340d1088ad7db4887d928028b61647d13019f3a
trangkimdat2005 <trangkimdatst2005@gmail.com>
2024-11-03 19:59:42 +0700

    version 1

---
+++ js/giaolapbaohanhjs.js
@@ -0,0 +1,67 @@
+
+// dia chi
+function openPopup() {
+    document.getElementById('popup').style.display = 'flex';
+    document.body.classList.add("overflow-hidden");
+}
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
+    document.body.classList.remove("overflow-hidden");
+}
+function chooseOption(option) {
+    document.querySelector('.choose-btn').textContent = option;
+    document.getElementById('popup').style.display = 'none';
+    document.body.classList.remove("overflow-hidden");
+}
+function closePopupOnOutsideClick(event) {
+    if (event.target.id === "popup") {
+        closePopup();
+    }
+}
+// tim kiem
+
+function showFind(){
+    document.getElementById('popup-find-all').style.display='flex';
+    document.body.classList.add("overflow-hidden");
+}
+function hideFind(){
+    document.getElementById('popup-find-all').style.display='none';
+    document.body.classList.remove("overflow-hidden");
+}
+function closePopupOnOutsideClickFind(event) {
+    if (event.target.id === "popup-find-all") {
+        document.getElementById('popup-find-all').style.display='none';
+        document.body.classList.remove("overflow-hidden");
+    }
+}
+// tai khoan
+function alertCall(){
+    var t=confirm("https://www.nguyenkim.com wants to open this application.")
+    if (t) {
+        window.location.href="index.html"
+    } else {
+        alert("You choose cancel!");
+    }
+}
+// email 
+
+var testEmail = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+var email = document.getElementById("Email").value;
+function checkEmail(){
+    var email = document.getElementById("Email").value;
+    if (email == "" || testEmail.test(email) == false){
+        document.getElementById('khonghople').style.display='flex';
+        document.getElementById('Email').style.border='1px red solid';
+    }
+    else {
+        document.getElementById('khonghople').style.display = 'none';
+        document.getElementById('Email').style.border = 'none';
+        
+        document.getElementById("Email").value = "";
+    }
+}
+function hideAlert(){
+    document.getElementById('Email').style.border='0';
+    document.getElementById('khongdungdinhdang').style.display='none';
+    document.getElementById('khonghople').style.display='none';
+}
---
+++ js/tonghopkhuyenmaijs.js
@@ -0,0 +1,75 @@
+// slide
+const rightbtn = document.querySelector('.fa-chevron-right');
+const leftbtn = document.querySelector('.fa-chevron-left');
+const imgNumber = document.querySelectorAll('.slide-khuyenmai_item');
+let current=0;
+rightbtn.addEventListener("click",function(){
+    current++
+    if(current>imgNumber.length-3){
+        current=0
+    }
+    document.querySelector(".slide-khuyenmai_center").style.right=current*50+"%"
+})
+leftbtn.addEventListener("click",function(){
+    current--
+    if(current<=0){
+        current=imgNumber.length-3
+    }
+    document.querySelector(".slide-khuyenmai_center").style.right=current*50+"%"
+})
+// locate
+function openPopup() {
+    document.getElementById('popup').style.display = 'flex';
+    document.body.classList.add("overflow-hidden");
+}
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
+    document.body.classList.remove("overflow-hidden");
+}
+function chooseOption(option) {
+    document.querySelector('.choose-btn').textContent = option;
+    document.getElementById('popup').style.display = 'none';
+    document.body.classList.remove("overflow-hidden");
+}
+function closePopupOnOutsideClick(event) {
+    if (event.target.id === "popup") {
+        closePopup();
+    }
+}
+// find
+
+function showFind(){
+    document.getElementById('popup-find-all').style.display='flex';
+    document.body.classList.add("overflow-hidden");
+}
+function hideFind(){
+    document.getElementById('popup-find-all').style.display='none';
+    document.body.classList.remove("overflow-hidden");
+}
+function closePopupOnOutsideClickFind(event) {
+    if (event.target.id === "popup-find-all") {
+        document.getElementById('popup-find-all').style.display='none';
+        document.body.classList.remove("overflow-hidden");
+    }
+}
+
+var testEmail = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+var email = document.getElementById("Email").value;
+function checkEmail(){
+    var email = document.getElementById("Email").value;
+    if (email == "" || testEmail.test(email) == false){
+        document.getElementById('khonghople').style.display='flex';
+        document.getElementById('Email').style.border='1px red solid';
+    }
+    else {
+        document.getElementById('khonghople').style.display = 'none';
+        document.getElementById('Email').style.border = 'none';
+        
+        document.getElementById("Email").value = "";
+    }
+}
+function hideAlert(){
+    document.getElementById('Email').style.border='0';
+    document.getElementById('khongdungdinhdang').style.display='none';
+    document.getElementById('khonghople').style.display='none';
+}
